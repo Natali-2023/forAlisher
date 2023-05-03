d411e2e2e197d9ab6f364ad6660965e945446f8f
Nataliia <pnata1978@gmail.com>
2023-05-03 13:36:24 +0200

    add homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        String[] arr = {"apple", "orange"};
+        String word = "orange";
+        System.out.println(getResultWord2(arr, word));
+
     }
+    public static boolean getResultWord2(String[] arr2, String word2){
+        boolean rezult=false;
+        for (int i=0; i<arr2.length; i++) {
+            if (arr2[i].equalsIgnoreCase(word2)) {
+                rezult = true;
+            }
+        }
+        return rezult;
+    }
+
 }
